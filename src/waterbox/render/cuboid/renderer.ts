d988324c667621660a0006711a06ef2dde6c38af
Vincent Wochnik <v.wochnik@gmail.com>
2026-05-15 14:15:12 +0200

    fix

--- src/waterbox/render/cuboid/renderer.ts
+++ src/waterbox/render/cuboid/renderer.ts
@@ -1,19 +1,8 @@
-import { RgbaColor, rgbaColorToString } from '../../color';
+import { Size, Rectangle, calculateRectAndSize, makePatteern, makeSteps } from '../util'; import { RgbaColor, rgbaColorToString } from '../../color';
 import { CuboidRenderingOptions } from '.';
-
-type Size = {
-  w: number;
-  h: number;
-};
-
-type Rectangle = {
-  x: number;
-  y: number;
-} & Size;
+import { BaseRenderingOptions } from '..';
 
 type PathFunction = (ctx: CanvasRenderingContext2D | OffscreenCanvasRenderingContext2D) => void;
-
-const DEFAULT_TILT_ANGLE = (Math.atan(1.0 / Math.sqrt(2.0)) * 180) / Math.PI;
 
 export function render(
   options: CuboidRenderingOptions,
@@ -24,7 +13,6 @@ export function render(
   const {
     width,
     height,
-    padding,
     value,
     clipEdges,
     scale,
@@ -35,7 +23,6 @@ export function render(
     waterPatternSource,
     frontPatternSource,
   } = options;
-  const tiltAngle = options.tiltAngle ?? DEFAULT_TILT_ANGLE;
   const scalePosition = options.scale?.position ?? 'back';
   const outerStrokeWidth = options.strokeWidths.outer;
   const innerStrokeWidth = options.strokeWidths.inner;
@@ -44,7 +31,7 @@ export function render(
   const waterPattern = makePatteern(bufferContext, waterPatternSource);
   const frontPattern = makePatteern(bufferContext, frontPatternSource);
 
-  const [rect, size] = calculateRectAndSize(width, height, padding, tiltAngle, outerStrokeWidth);
+  const [rect, size] = calculateRectAndSize(options);
 
   bufferContext.reset();
 
@@ -392,57 +379,4 @@ function outerPath(rect: Rectangle, size: Size, value: number): PathFunction {
 
     ctx.closePath();
   };
-}
-
-function calculateRectAndSize(
-  width: number,
-  height: number,
-  padding: number,
-  tiltAngle: number,
-  strokeWidth: number,
-): [Rectangle, Size] {
-  const angleRad = (tiltAngle * Math.PI) / 180;
-  const ratio = Math.sin(angleRad);
-
-  const halfMinSide = Math.min(width, height) / 2;
-  const maxPadding = halfMinSide - strokeWidth;
-  const actualPadding = Math.min(padding, maxPadding);
-
-  const innerWidth = width - 2 * actualPadding - strokeWidth;
-  const innerHeight = height - 2 * actualPadding - strokeWidth;
-
-  const actualHeight = innerHeight;
-  const actualWidth = ratio !== 0 ? Math.min(innerWidth, actualHeight / ratio) : innerWidth;
-
-  const rect: Rectangle = {
-    x: (width - actualWidth) / 2,
-    y: (height - actualHeight) / 2,
-    w: actualWidth,
-    h: actualHeight,
-  };
-  const size: Size = { w: rect.w, h: rect.w * ratio };
-  return [rect, size];
-}
-
-function makeSteps(divisions: number, value: number = 100): number[] {
-  const step = 100 / divisions;
-
-  const count = Math.max(0, Math.ceil(value / step) - 1);
-  const length = Math.min(count, divisions - 1);
-
-  return Array.from({ length }, (_, i) => step * (i + 1));
-}
-
-function makePatteern(
-  ctx: CanvasRenderingContext2D | OffscreenCanvasRenderingContext2D,
-  source?: CanvasImageSource,
-): CanvasPattern | undefined {
-  if (!source) {
-    return undefined;
-  }
-  const pattern = ctx.createPattern(source, 'repeat');
-  if (pattern === null) {
-    throw new Error('Failed to create pattern');
-  }
-  return pattern;
 }
--- src/waterbox/render/util.ts
+++ src/waterbox/render/util.ts
@@ -1,3 +1,17 @@
+import { BaseRenderingOptions } from ".";
+
+const DEFAULT_TILT_ANGLE = (Math.atan(1.0 / Math.sqrt(2.0)) * 180) / Math.PI;
+
+export type Size = {
+  w: number;
+  h: number;
+};
+
+export type Rectangle = {
+  x: number;
+  y: number;
+} & Size;
+
 export function createOffscreenRenderingContext(
   width: number,
   height: number,
@@ -8,4 +22,59 @@ export function createOffscreenRenderingContext(
     throw new Error("can't get context");
   }
   return context;
+}
+
+export function calculateRectAndSize(
+  {
+    width,
+    height,
+    padding,
+    tiltAngle = DEFAULT_TILT_ANGLE,
+    strokeWidths: { outer: strokeWidth }
+  }: BaseRenderingOptions
+): [Rectangle, Size] {
+  const angleRad = (tiltAngle * Math.PI) / 180;
+  const ratio = Math.sin(angleRad);
+
+  const halfMinSide = Math.min(width, height) / 2;
+  const maxPadding = halfMinSide - strokeWidth;
+  const actualPadding = Math.min(padding, maxPadding);
+
+  const innerWidth = width - 2 * actualPadding - strokeWidth;
+  const innerHeight = height - 2 * actualPadding - strokeWidth;
+
+  const actualHeight = innerHeight;
+  const actualWidth = ratio !== 0 ? Math.min(innerWidth, actualHeight / ratio) : innerWidth;
+
+  const rect: Rectangle = {
+    x: (width - actualWidth) / 2,
+    y: (height - actualHeight) / 2,
+    w: actualWidth,
+    h: actualHeight,
+  };
+  const size: Size = { w: rect.w, h: rect.w * ratio };
+  return [rect, size];
+}
+
+export function makeSteps(divisions: number, value: number = 100): number[] {
+  const step = 100 / divisions;
+
+  const count = Math.max(0, Math.ceil(value / step) - 1);
+  const length = Math.min(count, divisions - 1);
+
+  return Array.from({ length }, (_, i) => step * (i + 1));
+}
+
+export function makePatteern(
+  ctx: CanvasRenderingContext2D | OffscreenCanvasRenderingContext2D,
+  source?: CanvasImageSource,
+): CanvasPattern | undefined {
+  if (!source) {
+    return undefined;
+  }
+  const pattern = ctx.createPattern(source, 'repeat');
+  if (pattern === null) {
+    throw new Error('Failed to create pattern');
+  }
+  return pattern;
 }
